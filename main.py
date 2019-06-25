1c1a0559ec88316f300babefd9c3de6852d50c11
Syahrul Arifuddin <syahrulak98@gmail.com>
2019-06-26 00:29:30 +0700

    Add loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from random import randint
+import os
 
 def change_value(values):
   new_values = list()
@@ -22,16 +23,13 @@ def change_value(values):
       new_values.append(randint(0, 40))
     else:
       new_values.append(randint(60, 100))
-  
   return new_values
 
 def find_candidate(lower, upper, values):
   candidates = list()
-
   for value in values:
     if value > upper or value < lower:
       candidates.append(value)
-
   return candidates
 
 def fence(lower, upper, values, diff):
@@ -49,20 +47,25 @@ def outer_fences(lower, upper, values):
 
 def calculate_outlier(dataset_path):
   dataset = pd.read_csv(dataset_path)
-
-  transformed_data = change_value(dataset['Nilai'])
-  median_value = np.median(transformed_data)
-  lower_quantile = np.quantile(transformed_data, .25)
-  upper_quantile = np.quantile(transformed_data, .75)
-
-  minor_outlier = inner_fences(lower_quantile, upper_quantile, transformed_data)
-  major_outlier = outer_fences(lower_quantile, upper_quantile, transformed_data)
+  i = 0
+  while True:
+    transformed_data = change_value(dataset['Nilai'])
+    median_value = np.median(transformed_data)
+    lower_quantile = np.quantile(transformed_data, .25)
+    upper_quantile = np.quantile(transformed_data, .75)
+    minor_outlier = inner_fences(lower_quantile, upper_quantile, transformed_data)
+    major_outlier = outer_fences(lower_quantile, upper_quantile, transformed_data)
+    i += 1
+    if len(minor_outlier) > 0:
+      break
 
   print("\nAll dataset : {}".format(sorted(transformed_data)))
   print("Q1 data: {}".format(lower_quantile))
   print("Q3 data: {}".format(upper_quantile))
   print("Minor Outlier : {}".format(minor_outlier))
   print("Major Outlier : {}".format(major_outlier))
+  print("Stop at {}".format(i))
 
+print("Loading ....")
 calculate_outlier('dataset/MesinLearning.csv')
 calculate_outlier('dataset/GrafikaKomputer.csv')
